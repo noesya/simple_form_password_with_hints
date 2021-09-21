c60501bc970ea94d4d0fab93cca767119332ef08
pabois <pierreandre.boissinot@noesya.coop>
2021-09-21 14:47:54 +0200

    wip

---
+++ assets/javascripts/simple_form_password_with_hints.js
@@ -0,0 +1,52 @@
+/*global $, RegExp */
+$(function () {
+    'use strict';
+    var getCheckRegex = function (key, $check) {
+            var regex, min, chars;
+            switch (key) {
+            case 'uppercase':
+                regex = '[A-Z]';
+                break;
+            case 'lowercase':
+                regex = '[a-z]';
+                break;
+            case 'number':
+                regex = '[0-9]';
+                break;
+            case 'special':
+                // hyphen must be at the end
+                chars = $check.data('chars');
+                regex = '[' + chars + ']';
+                break;
+            case 'length':
+                min = $check.data('length');
+                regex = '.{' + min + ',128}';
+                break;
+            default:
+                break;
+            }
+            return regex;
+        },
+        performCheck = function ($container, key, password) {
+            var $check = $('.js-sfpwh-hint-' + key, $container),
+                regexKey = getCheckRegex(key, $check),
+                regex = new RegExp(regexKey);
+            if ($check.length) {
+                if (password.match(regex)) {
+                    $check.removeClass('sfpwh-hint--invalid');
+                } else {
+                    $check.addClass('sfpwh-hint--invalid');
+                }
+            }
+        };
+
+    $('.js-sfpwh-input').on('input', function () {
+        var $container = $(this).parents('.password_with_hints'),
+            checks = ['length', 'uppercase', 'lowercase', 'number', 'special'],
+            password = $(this).val(),
+            i;
+        for (i = 0; i < checks.length; i += 1) {
+            performCheck($container, checks[i], password);
+        }
+    });
+});
--- lib/simple_form_password_with_hints/password_with_hints_input.rb
+++ lib/simple_form_password_with_hints/password_with_hints_input.rb
@@ -3,11 +3,11 @@ class PasswordWithHintsInput < SimpleForm::Inputs::Base
 
   def input(wrapper_options = nil)
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-    merged_input_options[:class] << " js-password-with-hints-input "
-    template.content_tag :div, class: 'password-with-hints user-select-none js-password-with-hints' do
+    merged_input_options[:class] << " js-sfpwh-input "
+    # template.content_tag :div, class: 'sfpwh-container js-sfpwh-container' do
       @builder.password_field(attribute_name, merged_input_options) +
         password_uncloaking_div +
-        template.content_tag(:div, '', class: 'js-password-controls') do
+        template.content_tag(:div, '', class: 'sfpwh-controls js-sfpwh-controls') do
           [
             length_div,
             uppercase_div,
@@ -16,41 +16,57 @@ class PasswordWithHintsInput < SimpleForm::Inputs::Base
             special_char_div
           ].compact.join.html_safe
         end
-    end
+    # end
   end
 
   def password_uncloaking_div
-    template.content_tag(:i, '', class: 'far fa-eye password-toggle js-password-toggle') if options[:allow_password_uncloaking]
+    template.content_tag(
+      :i,
+      '',
+      class: 'far fa-eye password-toggle js-password-toggle'
+    ) if options[:allow_password_uncloaking]
   end
 
   def length_div
-    if should_display?(:length)
-      template.content_tag(:span, t('simple_form_password_with_hints.test_chars', length: options[:validators][:length]), class: 'password-hint password-hint--invalid password-hint-uppercase j-password-controls-length')
-    end
+    template.content_tag(
+      :span,
+      t('simple_form_password_with_hints.test_chars', min_length: options[:validators][:length]),
+      data: { length: options[:validators][:length] },
+      class: 'sfpwh-hint sfpwh-hint--invalid sfpwh-hint--uppercase js-sfpwh-hint-length'
+    ) if should_display?(:length)
   end
 
   def uppercase_div
-    if should_display?(:uppercase_char)
-      template.content_tag(:span, t('simple_form_password_with_hints.test_uppercase'), class: 'password-hint password-hint--invalid password-hint-uppercase j-password-controls-uppercase')
-    end
+    template.content_tag(
+      :span,
+      t('simple_form_password_with_hints.test_uppercase'),
+      class: 'sfpwh-hint sfpwh-hint--invalid sfpwh-hint--uppercase js-sfpwh-hint-uppercase'
+    ) if should_display?(:uppercase_char)
   end
 
   def lowercase_div
-    if should_display?(:lowercase_char)
-      template.content_tag(:span, t('simple_form_password_with_hints.test_lowercase'), class: 'password-hint password-hint--invalid password-hint-lowercase j-password-controls-lowercase')
-    end
+    template.content_tag(
+      :span,
+      t('simple_form_password_with_hints.test_lowercase'),
+      class: 'sfpwh-hint sfpwh-hint--invalid sfpwh-hint--uppercase js-sfpwh-hint-lowercase'
+    ) if should_display?(:lowercase_char)
   end
 
   def numeric_div
-    if should_display?(:numeric_char)
-      template.content_tag(:span, t('simple_form_password_with_hints.test_numeric'), class: 'password-hint password-hint--invalid password-hint-number j-password-controls-number')
-    end
+    template.content_tag(
+      :span,
+      t('simple_form_password_with_hints.test_numeric'),
+      class: 'sfpwh-hint sfpwh-hint--invalid sfpwh-hint--uppercase js-sfpwh-hint-number'
+    ) if should_display?(:numeric_char)
   end
 
   def special_char_div
-    if should_display?(:special_char)
-      template.content_tag(:span, t('simple_form_password_with_hints.test_special_char'), class: 'password-hint password-hint--invalid password-hint-special j-password-controls-special')
-    end
+    template.content_tag(
+      :span,
+      t('simple_form_password_with_hints.test_special_char'),
+      data: { chars: options[:validators][:special_char] },
+      class: 'sfpwh-hint sfpwh-hint--invalid sfpwh-hint--uppercase js-sfpwh-hint-special'
+    ) if should_display?(:special_char)
   end
 
   private
